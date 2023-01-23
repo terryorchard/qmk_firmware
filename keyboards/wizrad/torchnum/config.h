d4b3090546ed8ba4e62c3e4132864568f52786df
terryorchard <terryorchard@gmail.com>
2023-01-23 11:30:33 -0800

    Testing RGB

--- keyboards/wizrad/torchnum/config.h
+++ keyboards/wizrad/torchnum/config.h
@@ -3,24 +3,7 @@
 
 #pragma once
 
-#ifdef RGB_MATRIX_ENABLE
-#    define NOP_FUDGE 0.4
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 128
-#    define RGB_MATRIX_LED_COUNT 1
-#endif
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+#define NOP_FUDGE 0.4
+#define RGB_DI_PIN GP0
+#define RGB_MATRIX_LED_COUNT 1
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 128
--- keyboards/wizrad/torchnum/torchnum.c
+++ keyboards/wizrad/torchnum/torchnum.c
@@ -1,59 +1,45 @@
-/* Copyright 2022 splitkb.com <support@splitkb.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include "quantum.h"
 
-// The first four layers gets a name for readability, which is then used in the OLED below.
-enum layers {
-  _DEFAULT,
-  _LOWER,
-  _RAISE,
-};
-
-#ifdef RGB_MATRIX_ENABLE
 layer_state_t layer_state_set_user(layer_state_t state) {
     switch(biton32(state)) {
         case 1:
-            rgb_matrix_set_color_all(127, 0, 0);
+            rgb_matrix_set_color_all(RGB_RED);
             break;
         case 2:
-            rgb_matrix_set_color_all(0, 0, 127);
+            rgb_matrix_set_color_all(RGB_ORANGE);
+            break;
+        case 3:
+            rgb_matrix_set_color_all(RGB_GREEN);
+            break;
+        case 4:
+            rgb_matrix_set_color_all(RGB_BLUE);
+            break;
+        case 5:
+            rgb_matrix_set_color_all(RGB_TEAL);
+            break;
+        case 6:
+            rgb_matrix_set_color_all(RGB_PURPLE);
             break;
         default:
-            rgb_matrix_set_color_all(127, 0, 127);
+            rgb_matrix_set_color_all(RGB_MAGENTA);
             break;
     }
   return state;
-}
-#endif
+};
 
-
-/*
-layer_state_t layer_state_set_user(layer_state_t state) {
-  switch(biton32(state)) {
-    case 1:
-        rgb_matrix_set_color_all(255,0,0);
-        break;
-    case 2:
-        rgb_matrix_set_color_all(255,0,255);
-        break;
-    default:
-        rgb_matrix_set_color_all(255,0,255);
-        break;
+led_config_t g_led_config = {
+    {
+    // Key Matrix to LED Index
+        { 0,          NO_LED,     NO_LED,     NO_LED    },
+        { NO_LED,     NO_LED,     NO_LED,     NO_LED    },
+        { NO_LED,     NO_LED,     NO_LED,     NO_LED    },
+        { NO_LED,     NO_LED,     NO_LED,     NO_LED    },
+        { NO_LED,     NO_LED,     NO_LED    }
+    }, {
+    // LED Index to Physical Position
+        { 0,  0 }
+    }, {
+    // LED Index to Flag
+        1
     }
-    return state;
-}
-*/
+};
