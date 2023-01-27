bf812dea48561d35aac48d77f0152537576c3e78
terryorchard <terryorchard@gmail.com>
2023-01-27 12:31:10 -0800

    Adjusting keymap
    and still trying to get RGB working...

--- keyboards/wizrad/torchnum/config.h
+++ keyboards/wizrad/torchnum/config.h
@@ -3,10 +3,8 @@
 
 #pragma once
 
-#ifdef RGBLIGHT_ENABLE
-    #define NOP_FUDGE 0.4
-    #define RGBLED_NUM 1
-    #define RGBLIGHT_HUE_STEP 12
-    #define RGBLIGHT_SAT_STEP 8
-    #define RGB_DI_PIN GP0
-#endif
+#define NOP_FUDGE 0.4
+#define RGBLED_NUM 1
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGB_DI_PIN GP0
--- keyboards/wizrad/torchnum/torchnum.c
+++ keyboards/wizrad/torchnum/torchnum.c
@@ -19,46 +19,62 @@
 // The first four layers gets a name for readability, which is then used in the OLED below.
 
 enum layers {
-  _DEFAULT,
-  _LOWER,
-  _RAISE,
-  _NUMS,
-};
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
-        case _RAISE:
-            rgblight_setrgb(RGB_RED);
-            break;
-        case _LOWER:
-            rgblight_setrgb(RGB_GREEN);
-            break;
-        case _NUMS:
-            rgblight_setrgb(RGB_BLUE);
-            break;
-        default:
-            rgblight_setrgb(RGB_WHITE);
-            break;
-    }
-    return state;
+    _BASE,
+    _NAV,
+    _ALPHA,
+    _ARROW,
+    _MOUSE,
+    _NUMS,
+    _RGB
 };
 
 bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) {
         return false;
     }
-    if(IS_LAYER_ON(_LOWER) || IS_LAYER_ON(_RAISE) || IS_LAYER_ON(_NUMS)) {
+    if(IS_LAYER_ON(_ARROW) || IS_LAYER_ON(_RGB) || IS_LAYER_ON(_NUMS)) {
         if (clockwise){
-        tap_code(KC_WH_D);
-        } else{
         tap_code(KC_WH_U);
+        } else{
+        tap_code(KC_WH_D);
         }
     } else {
         if (clockwise){
-            tap_code(KC_VOLU);
-        } else{
             tap_code(KC_VOLD);
+        } else{
+            tap_code(KC_VOLU);
         }
     }
     return true;
-}
+};
+
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (biton32(state)) {
+        case _BASE:
+            rgblight_setrgb_at(255,0,255,0);
+            break;
+        case _NAV:
+            rgblight_setrgb_at(127,127,127,0);
+            break;
+        case _ALPHA:
+            rgblight_setrgb_at(255,0,0,0);
+            break;
+        case _ARROW:
+            rgblight_setrgb_at(0,255,0,0);
+            break;
+        case _MOUSE:
+            rgblight_setrgb_at(0,0,255,0);
+            break;
+        case _NUMS:
+            rgblight_setrgb_at(127,0,127,0);
+            break;
+        case _RGB:
+            rgblight_setrgb_at(0,127,255,0);
+            break;
+        default:
+            rgblight_setrgb_at(255,255,255,0);
+            break;
+    }
+    return state;
+};
