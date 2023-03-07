eaa48dbae78aec9459e0c78e86ba65517e82d242
terryorchard <terryorchard@gmail.com>
2023-03-06 17:53:06 -0800

    New Corne layout

--- keyboards/wizrad/wizergo/keymaps/torch/keymap.c
+++ keyboards/wizrad/wizergo/keymaps/torch/keymap.c
@@ -68,10 +68,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                   KC_BTN2,			KC_BTN1,			KC_BSPC,			KC_TRNS,      KC_TRNS,      KC_LALT,			KC_PDOT,			KC_PCMM
   ),
   [_GAMING] = LAYOUT(           // 6
-    KC_TAB,			  KC_Q,				  KC_W,				  KC_E,				  KC_R,				  KC_2,				  KC_3,				  KC_4,				  KC_5,				  KC_6,
-    KC_LCTL,			KC_A,				  KC_S,				  KC_D,				  KC_F,				  KC_H,				  KC_J,				  KC_K,				  KC_L,				  KC_CAPS,
-    KC_LSFT,			KC_Z,				  KC_X,				  KC_C,				  KC_V,				  KC_N,				  KC_M,				  KC_COMM,			KC_DOT,			  TO(0),
-                  KC_ESC,			  KC_1,				  KC_SPC,			  KC_TRNS,			KC_TRNS,			KC_TRNS,			KC_TRNS,			KC_TRNS
+    KC_TAB,			  KC_Q,				  KC_W,				  KC_E,				  KC_R,				  KC_T,				  KC_Y,				  KC_U,				  KC_I,				  KC_O,
+    KC_LCTL,			KC_A,				  KC_S,				  KC_D,				  KC_F,				  KC_G,				  KC_H,				  KC_J,				  KC_K,				  KC_CAPS,
+    KC_LSFT,			KC_Z,				  KC_X,				  KC_C,				  KC_V,				  KC_B,				  KC_N,				  KC_M,			    KC_DOT,			  TO(0),
+                  KC_ESC,			  KC_2,				  KC_SPC,			  KC_TRNS,			KC_TRNS,			KC_1,			    KC_3,			    KC_0
   )
 };
 
